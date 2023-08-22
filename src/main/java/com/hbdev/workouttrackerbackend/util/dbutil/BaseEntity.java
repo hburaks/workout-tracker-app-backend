26759b7b4a16c5bc4b6d5312cb9eea1fb71bd5a6
Hasan Burak Songur <123594944+hburaks@users.noreply.github.com>
2023-08-22 14:45:15 +0300

    Add createdBy to base entity

--- src/main/java/com/hbdev/workouttrackerbackend/util/dbutil/BaseEntity.java
+++ src/main/java/com/hbdev/workouttrackerbackend/util/dbutil/BaseEntity.java
@@ -4,9 +4,12 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Date;
 import java.util.UUID;
@@ -30,8 +33,21 @@ public class BaseEntity {
     @LastModifiedDate
     private Date lastModifiedDate;
 
+    @CreatedBy
+    private String createdBy;
     @PrePersist
     protected void prePersist() {
         this.uuid = UUID.randomUUID();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String username = "";
+        if (authentication == null || !authentication.isAuthenticated()) {
+            username = "anonymous";
+        } else {
+            username = authentication.getPrincipal().toString();
+        }
+
+        setCreatedBy(username);
+
     }
 }
