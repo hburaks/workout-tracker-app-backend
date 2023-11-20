f3c8655cfdadfc3d526aa56e0d914743a73fd1f8
Hasan Burak Songur <hasanburaksongur@gmail.com>
2023-11-21 00:11:33 +0300

    Change bootstrapt to tailwind and update header

---
+++ workout-tracker-frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+      fontFamily: {
+        sans: ['ui-sans-serif', 'system-ui'],
+        serif: ['ui-serif', 'Georgia'],
+        mono: ['ui-monospace', 'SFMono-Regular'],
+        display: ['Oswald'],
+        body: ['"Open Sans"'],
+      }
+  },
+  plugins: [],
+  
+};
